adcf4cbd6dddba3e4ea95e49a67ef003a82557e5
AlexTatarov <tatarovalexandru@gmail.com>
2017-10-10 19:19:00 +0200

    Added Modal Component

--- ng/client/maps-app/src/app/app.module.ts
+++ ng/client/maps-app/src/app/app.module.ts
@@ -4,22 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { GoogleMapsComponent } from './google-maps/google-maps.component';
+import { LoginModalComponent } from './login-modal/login-modal.component';
 
 import { AgmCoreModule } from 'angular2-google-maps/core';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
 
 const googleMapsCore = AgmCoreModule.forRoot({
   apiKey : 'AIzaSyA0XBPNoJ_DDPdygliKaVftuAYKzFjMdSw',
 });
 
+const loginModalCore = ModalModule.forRoot();
+
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    GoogleMapsComponent
+    GoogleMapsComponent,
+    LoginModalComponent
   ],
   imports: [
     BrowserModule,
-    googleMapsCore
+    googleMapsCore,
+    loginModalCore
   ],
   providers: [],
   bootstrap: [AppComponent]
