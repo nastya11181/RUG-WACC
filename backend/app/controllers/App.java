264d5fb4a1107a547f3ee8efb71b828100d45673
Nastya <Nastya>
2017-10-16 03:33:12 +0300

    async driver added

--- backend/app/controllers/App.java
+++ backend/app/controllers/App.java
@@ -1,98 +1,79 @@
-package controllers;
+package app.controllers;
 
 import java.util.Set;
 
-import javax.ejb.EJB;
-import javax.ejb.Stateless;
-
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
-import com.mongodb.DBCursor;
-import com.mongodb.MongoClient;
-import com.mongodb.util.JSON;
-import com.mongodb.client.FindIterable;
-
-import java.net.UnknownHostException;
-import java.util.Date;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoException;
 import play.mvc.*;
 import javax.inject.Inject;
-import controllers.MongoClientProvider;
 
 
-/**
- * Java + MongoDB Hello world Example
- * 
- */
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+import com.mongodb.reactivestreams.client.MongoCollection;
+import com.mongodb.reactivestreams.client.MongoDatabase;
+import org.bson.Document;
+import static com.mongodb.client.model.Filters.eq;
+import com.mongodb.client.model.UpdateOneModel;
+import com.mongodb.client.result.UpdateResult;
+
+
+import app.services.SubscriberHelpers.*;
+import app.services.MongoClientProvider;
+
+
 
 public class App extends Controller {	
 	
 	private MongoClientProvider mongoClientProvider;
 	
-// constructor based injector	
+	// constructor based injector	
 	@Inject
 	public App(MongoClientProvider mongoClientProvider){
 		this.mongoClientProvider = mongoClientProvider; 
 	}
 
-
 	
-
-	public Result getCollectionNames(){	
-	
-		/**** Get database ****/
-		// if database doesn't exist, MongoDB will create it for you
-		DB db = mongoClientProvider.get().getDB("uber");
+	public Result setUnavailable(String id){
+		MongoDatabase db = mongoClientProvider.get().getDatabase("uber");		
+		MongoCollection<Document> coll = db.getCollection("bikes");
 		
-		/**** Get collection / table from 'testdb' ****/
-		// if collection doesn't exists, MongoDB will create it for you
-		DBCollection table = db.getCollection("bikes");
-		
-		Set<String> colls = db.getCollectionNames();
-		
-		for (String s : colls) {
-		    System.out.println(s);
-		}	
-		
-		
+		coll.updateOne(eq("id", id), new Document("$set", new Document("status", "unavailable")))
+			.subscribe(new PrintSubscriber<UpdateResult>("Update Result: %s"));
 		return ok(views.html.index.render());
-					
-	}
-	
-	public Result getAllBikes(){
-		DB db = mongoClientProvider.get().getDB("uber");		
-		DBCollection coll = db.getCollection("bikes");		
-		DBCursor cursor = coll.find();
-		while (cursor.hasNext()){
-			System.out.println(cursor.next());
-		}
-//		subscriber = printDocumentSubscriber();
-//		coll.find().subscribe(subscriber);
-//		subscriber.awaitTerminalEvent();
 		
-		return ok(views.html.index.render());
 	}
 	
 	public Result getAvailableBikes(){
-		DB db = mongoClientProvider.get().getDB("uber");		
-		DBCollection coll = db.getCollection("bikes");	
-		BasicDBObject whereQuery = new BasicDBObject();
-		whereQuery.put("status", "available");
-		DBCursor cursor = coll.find(whereQuery);
-		while (cursor.hasNext()){
-			System.out.println(cursor.next());
-		}
+		MongoDatabase db = mongoClientProvider.get().getDatabase("uber");		
+		MongoCollection<Document> coll = db.getCollection("bikes");
+		coll.find(eq("status", "available")).subscribe(new PrintDocumentSubscriber());
+		
+		
+		//BasicDBObject whereQuery = new BasicDBObject();
+		//whereQuery.put("status", "available");
+		//DBCursor cursor = coll.find(whereQuery);
+		//while (cursor.hasNext()){
+		//	System.out.println(cursor.next());
+		//}
 		return ok(views.html.index.render());
 		
 		
 	}
+	
+//	public Result getAllBikes(){
+//		MongoDatabase db = mongoClientProvider.get().getDatabase("uber");		
+//		MongoCollection<Document> coll = db.getCollection("bikes");	
+//		
+//		DBCursor cursor = coll.find();
+//		while (cursor.hasNext()){
+//			System.out.println(cursor.next());
+//		}
+//
+//		return ok(views.html.index.render());
+//	}
+	
+
+	
+
 
 
 	
--- backend/app/services/MongoClientProvider.java
+++ backend/app/services/MongoClientProvider.java
@@ -1,9 +1,10 @@
-package controllers;
+package app.services;
 
 import javax.inject.Singleton;
+//import com.mongodb.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
 
-import com.mongodb.MongoClient;
-import com.mongodb.async.client.MongoClients;
 
 @Singleton
 public class MongoClientProvider {
@@ -12,8 +13,8 @@ public class MongoClientProvider {
 
 	public MongoClient get() {
 		if (mongoClient == null){
-			mongoClient = new MongoClient("localhost", 27017);
-			//mongoClient = MongoClients.create();
+			//mongoClient = new MongoClient("localhost", 27017);
+			mongoClient = MongoClients.create();
 			System.out.println("New mongo client created");
 		}
 				
