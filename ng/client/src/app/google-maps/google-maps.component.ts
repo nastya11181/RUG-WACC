7f6524390d8daa5efb910e7b3819e234ad7c3f66
AlexTatarov <tatarovalexandru@gmail.com>
2017-10-30 22:32:58 +0100

    Reverse Geocoding

--- ng/client/src/app/google-maps/google-maps.component.ts
+++ ng/client/src/app/google-maps/google-maps.component.ts
@@ -3,20 +3,30 @@ import { Component, OnInit, Injectable } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 
+import { GeoService } from './GeoService';
+
+
 @Component({
   selector: 'app-google-maps',
   templateUrl: './google-maps.component.html',
-  styleUrls: ['./google-maps.component.css']
+  styleUrls: ['./google-maps.component.css'],
+  viewProviders : [GeoService]
+
 })
 
 @Injectable()
 export class GoogleMapsComponent implements OnInit {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private geoService: GeoService) { }
   lat: number = 53.22;
   long: number = 6.55;
   zoom: number = 12;
   draggable: boolean = true;
+
+  private location = "Cuza-Voda 24, Chisinau, Moldova";
+  result;
+
+
 
   private url: string = 'api/bikes';
   bikeMarkers;
@@ -27,11 +37,18 @@ export class GoogleMapsComponent implements OnInit {
         this.bikeMarkers = bikes;
         console.log(this.bikeMarkers);
     });
+    this.getAddress();
+    }
 
 
-  }
+    getAddress(): void {
+        this.geoService.getLocation(53.22,6.55)
+            .subscribe((response) => {
+              this.result = response.results[0];
+              console.log(this.result);
+            })
 
-
+          }
 
 
     clickedMarker(label: string, index: number){
