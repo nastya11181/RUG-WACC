a3e4da12720d0ae4d9263be05a801545e432e583
AlexTatarov <tatarovalexandru@gmail.com>
2017-11-03 01:05:01 +0100

    Table of available bikes

--- ng/client/src/app/google-maps/google-maps.component.ts
+++ ng/client/src/app/google-maps/google-maps.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, Injectable } from '@angular/core';
 
-
 import { HttpClient } from '@angular/common/http';
 
 import { GeoService } from './GeoService';
 
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-google-maps',
@@ -17,38 +17,71 @@ import { GeoService } from './GeoService';
 @Injectable()
 export class GoogleMapsComponent implements OnInit {
 
-  constructor(private http: HttpClient, private geoService: GeoService) { }
+  constructor(private http: HttpClient, private geoService: GeoService) {
+
+   }
   lat: number = 53.22;
   long: number = 6.55;
   zoom: number = 12;
   draggable: boolean = true;
 
-  private location = "Cuza-Voda 24, Chisinau, Moldova";
   result;
 
 
 
   private url: string = 'api/bikes';
   private bikeMarkers: marker[];
+  private bikeys = [];
+  private bikey: id_ad_co;
+  private counter : number;
+  private bikeAddress : id_ad_co[];
+  private numbers = [];
   ngOnInit():void {
     this.http.get<marker[]>(this.url)
       .subscribe(bikes => {
         this.bikeMarkers = bikes;
         console.log(this.bikeMarkers);
         console.log(this.bikeMarkers.length);
-
         for(let i of this.bikeMarkers)
         {
           if(i.status)
           {
-            console.log(this.getAddress(i.coords.x,i.coords.y));
+            this.getAddress(i.coords.x,i.coords.y).subscribe(val =>
+            {
+              //console.log(this.bikey);
+              this.bikey = {
+                id: i.id.$oid,
+                address: val,
+                coords: {
+                  x: i.coords.x,
+                  y: i.coords.y
+                }
+              }
+
+              this.bikeys.push(this.bikey);
+              //console.log("Log all bikes");
+              //console.log(this.bikeys);
+            });
           }
         }
-
     });
 
+    /*
+    this.getAddress(53.22,6.55).subscribe(val => {
+      console.log(val);
+    });
 
-    this.getAddress(53.22,6.55);
+    let btn = document.getElementsByClassName("coolbutton");
+    btn.addEventListener("click",(e:Event)=>{
+      console.log("Pressed some button");
+    })
+    */
+    }
+    selectBike(mickey: id_ad_co):void{
+      var body = {id : mickey.address,coords: mickey.coords};
+      console.log("Set bike with " + mickey.address + " id availability to 'false'");
+      this.http.post(this.url+"/"+mickey.id, body).subscribe();
+      //http call to update but status from free to reserved;
     }
     /*
     createTable():void{
@@ -60,14 +93,12 @@ export class GoogleMapsComponent implements OnInit {
     }
     */
 
-    getAddress(lat:number,lng:number): void {
-        this.geoService.getLocation(lat,lng)
-            .subscribe((response) => {
-              this.result = response.results[0];
-
-              console.log(this.result.formatted_address);
-            })
-          }
+    getAddress(lat:number,lng:number): Observable<string> {
+      return this.geoService.getLocation(lat,lng)
+        .map((response) => {
+          return this.result = response.results[0].formatted_address;
+        })
+      }
 
 
     clickedMarker(label: string, index: number){
@@ -102,4 +133,13 @@ export class GoogleMapsComponent implements OnInit {
      y: number;
    }
    status: boolean;
+ };
+
+ interface id_ad_co {
+   id : string;
+   address: string;
+   coords: {
+     x: number;
+     y: number;
+   }
  }
